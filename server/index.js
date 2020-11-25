13d6ca0312f9661dde3cf2cc807b873214c92d7c
Brooke Miller <brookecm28@gmail.com>
2020-11-25 14:13:43 -0700

    Lecture Day 2 Complete

--- server/index.js
+++ server/index.js
@@ -2,11 +2,19 @@ const express = require('express')
 const app = express()
 const usersCtrl = require('./usersController')
 
+app.use(express.json()) //this is called (a top-level) Middleware and will be required for all servers moving forward
+//every single request that passes through our server will pass through this middleware
+//JSON parses the body
+//this allows us to use .put and .post
+
 app.get('/test', (req, res) => {
     res.send('HELLO IS IT ME YOURE LOOKING FOR?')
 })
 
 app.get('/api/users', usersCtrl.getAllUsers)
 app.get('/api/users/:user_id', usersCtrl.getSingleUser) //:user_id is a param!
+app.post('/api/users', usersCtrl.addUser) //post will add!
+app.put('/api/users/:user_id', usersCtrl.editUser) //param is needed so we can target a specific user rather than the entire database (or array in this case)
+app.delete('/api/users/:user_id', usersCtrl.deleteUser)
 
 app.listen(4444, ()=>console.log('Server listening on port 4444'))
--- server/usersController.js
+++ server/usersController.js
@@ -1,4 +1,5 @@
 const users = require('../data.json')
+let id = users[users.length - 1].id + 1
 
 module.exports = {
     getAllUsers: (req, res) => {
@@ -23,7 +24,51 @@ module.exports = {
         } else {
             res.status(404).send('User does not exist')
         }
-
         res.send('USER')
+    },
+    addUser: (req, res) => {
+        const {first_name, last_name, email} = req.body //this sets the rule that when someone asks to make a new user, the body needs to be an object with these properties (does this via destructuring, since that's what we're doing here)
+        const newUser = {
+            id,
+            first_name, //this is a shorthand way of assigning these values to these properties
+            last_name,
+            email,
+        }
+        users.push(newUser)
+
+        id++
+
+        res.status(200).send(users)
+    },
+    editUser: (req, res) => {
+        const {user_id} = req.params
+        const {first_name, last_name, email} = req.body
+        const index = users.findIndex(element => element.id === +user_id)
+        if (index === -1) {
+            return res.status(404).send('User does not exist.')
+        } 
+        
+        const existingUser = users[index]
+
+        users[index] = {
+            id: existingUser.id,
+            first_name: first_name || existingUser.first_name,
+            last_name: last_name || existingUser.last_name,
+            email: email || existingUser.email,
+        }
+
+        res.status(200).send(users)
+    },
+    deleteUser: (req, res) => {
+        const {user_id} = req.params
+        const index = users.findIndex((element) => element.id === +user_id)
+
+        if(index === -1) {
+            return res.status(404).send('User does not exist')
+        }
+
+        users.splice(index, 1)
+        res.status(200).send(users)
+
     },
 }
